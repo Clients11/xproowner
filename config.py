5eaf94838db6c58dab8e24ad49e4da59d763372e
Clients11 <166890312+Clients11@users.noreply.github.com>
2025-05-22 12:43:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,8 +24,12 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", 60*60*2)) # 2 hours
 VIDEO_DURATION_LIMIT = int(getenv("VIDEO_DURATION_LIMIT", 60*20)) # 20 minutes
 
 # Vars For API End Pont.
-YTPROXY_URL = getenv("YTPROXY_URL", 'http://tubed.okflix.top') ## E.G https://yt.okflix.
-YT_API_KEY = "freekey15052025"
+#YTPROXY_URL = getenv("YTPROXY_URL", 'http://tubed.okflix.top') ## E.G https://yt.okflix.
+#YT_API_KEY = "freekey15052025"
+
+API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
+API_KEY = getenv("API_KEY", '30DxNexGenBotsc0db7b') # 
+
 # Chat id of a group for logging bot's activities
 
 LOGGER_ID = int(getenv("LOGGER_ID"))
